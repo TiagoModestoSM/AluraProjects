0e6ac1552bf25289cd747241982c1a8a2bfa923b
Labigo D Piaui <tiago.moura@ufpi.edu.br>
2024-08-27 23:32:16 -0300

    Numero Limite

--- Numero Secreto/app.js
+++ Numero Secreto/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = []
-let numeroLimite = 100;
+let numeroLimite = 10;
 
 let NumeroSecreto = NumeroAleatorio(); //Variavel do numero aleatorio
 let Tentativas = 1; // Numero de tentativas
